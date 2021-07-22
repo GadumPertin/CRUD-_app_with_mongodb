eca670cba07a0e4eda15f9a5f406fe5b6f0b96cf
Gadum Pertin <gadumpertin25@gmail.com>
2021-07-22 13:29:39 +0530

    done done

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
-const https = require('http');
-const app = require('./app')
+const app = require('./app');
+const https = require('http')
 const port = process.env.PORT || 5000;
 const server = https.createServer(app);
 server.listen(port);
